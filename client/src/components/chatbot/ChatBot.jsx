58865bd916f786e467448a6bfe674c32d040d2b8
kisat0 <ghanembenyoucef@gmail.com>
2024-03-26 12:58:45 +0100

    feat: add chatbot

--- client/src/components/chatbot/ChatBot.jsx
+++ client/src/components/chatbot/ChatBot.jsx
@@ -6,41 +6,56 @@ const ChatBotFrame = () => {
     <iframe
       src="https://www.chatbase.co/chatbot-iframe/UE3hxvd1dv4bbFSTMgjWQ"
       title="Chatbot"
-      width="40%"
       className="chatbot-iframe"
-      style={{ height: "100%", minHeight: "700px", border: "none" }}
     ></iframe>
   );
 };
 
 export const ChatBot = () => {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const openChatbot = () => {
-        setIsOpen(true);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const openChatbot = () => {
+    setIsOpen(true);
+  };
+
+  const closeChatbot = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const chatbotContainer = document.querySelector(".chatbot-container");
+      if (
+        chatbotContainer &&
+        !chatbotContainer.contains(event.target) &&
+        isOpen &&
+        event.target.className !== "chatbot-button"
+      ) {
+        closeChatbot();
+      }
     };
+    document.addEventListener("click", handleClickOutside);
 
-    const closeChatbot = () => {
-        setIsOpen(false);
-    }
-
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (event.target.className === "chatbot-container") {
-                closeChatbot();
-            }
-        };
-
-        document.addEventListener("click", handleClickOutside);
-
-        return () => {
-            document.removeEventListener("click", handleClickOutside);
-        }
-    }
-    , []);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-      <div className="chatbot-button" onClick={openChatbot}>
-          Open
+    <>
+      <div className="chatbot-page-container">
+        <div className="chatbot-container">
+          {!isOpen && (
+            <div className="chatbot-button" onClick={openChatbot}>
+              <span className="material-symbols-outlined">chat</span>
+            </div>
+          )}
+          {isOpen && (
+            <div className="chatbot-content">
+              <ChatBotFrame />
+            </div>
+          )}
+        </div>
       </div>
+    </>
   );
 };
