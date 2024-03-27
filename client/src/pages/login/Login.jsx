493afd4c00b4fea54198aced0844bb61625a9bf8
mathiszerari <mathiszerari@icloud.com>
2024-03-27 10:30:38 +0100

    :bug: material theme provider bug fixed

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,12 +1,23 @@
 import FormLogin from "../../components/formLogin/FormLogin";
 import "./Login.scss";
 
+import { ThemeProvider, createTheme } from "@mui/material";
+
 const Login = () => {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
     <div className="white">
       <h1>Login</h1>
       <div className="containeur-login">
-        <FormLogin />
+
+        <ThemeProvider theme={darkTheme}>
+          <FormLogin />
+        </ThemeProvider>
       </div>
     </div>
   )
--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,12 +1,23 @@
 import FormSignup from "../../components/formSignup/FormSignup";
 import "./Signup.scss";
 
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+
 const Signup = () => {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
     <div className="white">
       <h1>Signup</h1>
       <div className="containeur-signup">
-        <FormSignup />
+
+        <ThemeProvider theme={darkTheme}>
+          <FormSignup />
+        </ThemeProvider>
       </div>
     </div>
   )
