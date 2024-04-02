10e92d24c4943be40586ce0a697c11e8efbe682f
Noé HLM <noeheylemadec@gmail.com>
2024-04-02 16:55:33 +0200

    fix

--- client/src/components/serieDetails/SerieDetails.jsx
+++ client/src/components/serieDetails/SerieDetails.jsx
@@ -1,23 +1,37 @@
-import "./SerieDetails.scss"
-import { useTheme } from "@mui/material"
+import { useNavigate } from 'react-router-dom';
+
 var json = require("../../utils/fr.json");
 
-const SerieDetails = () => {
+import { useTheme } from "@mui/material"
+import "./SerieDetails.scss"
 
+const SerieDetails = ({ episode }) => {
     const theme = useTheme().palette;
 
+    const navigate = useNavigate();
+
+    const dateString = episode.releaseDate;
+    const date = new Date(dateString);
+
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = date.toLocaleDateString('fr-FR', options);
+
+    const openEpisode = async () => {
+        navigate(`/watch/${episode._id}`);
+    };
+
     return (
-        <div className="episode-details">
-            <img src="https://static.bandainamcoent.eu/high/jujutsu-kaisen/jujutsu-kaisen-cursed-clash/00-page-setup/JJK-header-mobile2.jpg" alt="jujutsu kaisen" />
-            <div className="episode-txt">
+        <div className="episode-details" onClick={openEpisode}>
+            <img src={episode.thumbnail} alt="jujutsu kaisen" />
+            <div className="episode-txt" style={{ background: theme.background.episodeDetailsBackground}}>
                 <div className="episode-infos">
                     <div className="episode-description" >
-                        <h1 style={{ color: theme.text.primary }}>{json.summary.Episode} </h1>
-                        <p style={{ color: theme.text.grey }}>{json.summary.Description}</p>
+                        <h1 style={{ color: theme.text.primary }}>{episode.title}</h1>
+                        <p style={{ color: theme.text.grey }}>{episode.description}</p>
                     </div>
                     <div className="episode-time" style={{ color: theme.text.secondary }}>
-                        <p style={{ color: theme.text.grey }}>{json.summary.Duration} 24m</p>
-                        <p style={{ color: theme.text.grey }}>25 avril 2021</p>
+                        <p style={{ color: theme.text.grey }}>{json.summary.Duration} {episode.duration}</p>
+                        <p style={{ color: theme.text.grey }}>{formattedDate}</p>
                     </div>
                 </div>
             </div>
@@ -25,7 +39,6 @@ const SerieDetails = () => {
     );
 }
 export default SerieDetails;
-
 
 
 
