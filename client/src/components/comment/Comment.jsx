5357bbde475a5dbc798d60681f8b86b55cded23c
mathiszerari <mathiszerari@icloud.com>
2024-04-05 16:13:54 +0200

    :lipstick: comment actions

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -6,9 +6,20 @@ function Comment() {
 
   return (
     <div className='comment-container'>
-      <h1 className='comment-logo'>AnimeNation</h1>
+      <h1 className='comment-title'>Commentaire</h1>
 
-      <p>Le site animeNation.com n'héberge aucun fichier vidéo, que ce soit pour du streaming ou du téléchargements. Celui-ci consiste en une indexation de liens vidéos sur différents hébergeurs publiques et connus.</p>
+      <div className='comment-content'>
+        <div className='comment-infos-top'>
+          <p className='comment-author'>Auteur</p>
+          <p className='comment-date'>Date</p>
+        </div>
+        <p className='comment-text'>Cet épisode est le meilleure épisode de la saison je veux rien savoir</p>
+        <div className='comment-infos-bottom'>
+          <p className='comment-like'>Like</p>
+          <p className='comment-dislike'>Dislike</p>
+          <button className='comment-reply'>Reply</button>
+        </div>
+      </div>
     </div>
   );
 }
