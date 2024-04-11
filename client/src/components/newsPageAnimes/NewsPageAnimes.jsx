6cacbe2680458ab188dc60a67a9d56023f72dc9a
kisat0 <ghanembenyoucef@gmail.com>
2024-04-11 17:31:16 +0200

    feat: update news page

--- client/src/components/newsPageAnimes/NewsPageAnimes.jsx
+++ client/src/components/newsPageAnimes/NewsPageAnimes.jsx
@@ -1,50 +1,65 @@
 import { useTheme } from "@mui/material";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import json from "../../utils/fr.json";
 import "./NewsPageAnimes.scss";
 
-const NewsPageAnimes = ({ anime, season, number, thumbnail, _id, onClick }) => {
+const NewsPageAnimes = ({
+  anime,
+  season,
+  number,
+  thumbnail,
+  _id,
+  onClick,
+  onMouseEnter,
+}) => {
+  const [dataAnime, setDataAnime] = useState([]);
+  const theme = useTheme().palette;
 
-    const [dataAnime, setDataAnime] = useState([]);
-    const theme = useTheme().palette;
+  const handleOnClick = () => {
+    onClick(anime);
+  };
 
-    const handleOnClick = () => {
-        onClick(anime); 
-    };
+  const getAnime = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ADDRESS}/animes/${anime}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch anime");
+      }
+      const dataAnime = await response.json();
+      setDataAnime(dataAnime);
+    } catch (error) {
+      console.error("Error fetching anime:", error);
+    }
+  };
 
+  useEffect(() => {
+    getAnime();
+  }, []);
 
-    const getAnime = async () => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_ADDRESS}/animes/${anime}`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch anime');
-            }
-            const dataAnime = await response.json();
-            setDataAnime(dataAnime);
-        } catch (error) {
-            console.error('Error fetching anime:', error);
-        }
-    };
-
-    useEffect(() => {
-        getAnime();
-    }, []);
-
-
-    return (
-        <div className="NewsContainer" onClick={handleOnClick}>
-            <div className='ImgNewsCompo'>
-                <img className="imgNewsComponent" src={thumbnail} alt="jujutsu kaisen" />
-            </div>
-            <div className="NewsInfos">
-                <h1>{dataAnime.title}</h1>
-                <div className="EpisodeData">
-                    <p>S{season}</p>
-                    <p>Episode {number}</p>
-                </div>
-            </div>
+  return (
+    <div
+      className="NewsContainer"
+      onClick={handleOnClick}
+      onMouseEnter={onMouseEnter}
+    >
+      <div className="ImgNewsCompo">
+        <img
+          className="imgNewsComponent"
+          src={thumbnail}
+          alt="jujutsu kaisen"
+        />
+      </div>
+      <div className="NewsInfos">
+        <h1>{dataAnime.title}</h1>
+        <div className="EpisodeData">
+          <p>S{season}</p>
+          <p>Episode {number}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NewsPageAnimes;
--- client/src/pages/news/News.jsx
+++ client/src/pages/news/News.jsx
@@ -1,65 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import NewsPageAnimes from "../../components/newsPageAnimes/NewsPageAnimes";
 import json from "../../utils/fr.json";
 import "./News.scss";
+import Loader from "../../components/loader/loader";
 
 const News = () => {
-    const [data, setData] = useState([]);
-    const [anime, setAnime] = useState("6604985745954d85e7d15b00");
-    const [dataA, setDataA] = useState([]);
+  const [data, setData] = useState([]);
+  const [anime, setAnime] = useState("6604985745954d85e7d15b00");
+  const [dataA, setDataA] = useState([]);
 
+  const getNewsEpisodes = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ADDRESS}/episodes/news`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch episodes");
+      }
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error("Error fetching episodes:", error);
+    }
+  };
 
-    const getNewsEpisodes = async () => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_API_ADDRESS}/episodes/news`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch episodes');
-            }
-            const data = await response.json();
-            setData(data);
-        } catch (error) {
-            console.error('Error fetching episodes:', error);
-        }
-    };
+  const handleNewsPageAnimesClick = async (anime) => {
+    try {
+      setAnime(anime);
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ADDRESS}/animes/${anime}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch anime");
+      }
+      const dataA = await response.json();
+      setDataA(dataA);
+    } catch (error) {
+      console.error("Error fetching anime:", error);
+    }
+  };
 
-    const handleNewsPageAnimesClick = async (anime) => {
-        try {
-            setAnime(anime);
-            const response = await fetch(`${process.env.REACT_APP_API_ADDRESS}/animes/${anime}`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch anime');
-            }
-            const dataA = await response.json();
-            setDataA(dataA);
-        } catch (error) {
-            console.error('Error fetching anime:', error);
-        }
-    };
+  useEffect(() => {
+    getNewsEpisodes();
+    handleNewsPageAnimesClick(anime);
+  }, []);
 
-    useEffect(() => {
-        getNewsEpisodes();
-       handleNewsPageAnimesClick(anime);
-    }, []);
+  if (!anime || !dataA || !data) return <Loader />;
 
-    return (
-        <div className="NewsPage">
-            <Navbar />
-            <img className="backgroundNews" src= {dataA.thumbnail} />
+  return (
+    <div className="NewsPage">
+      <Navbar />
 
-
-            <div className="NewsTitleDesc">
-                <h1>{dataA.title}</h1>
-                <p>{dataA.description}</p>
-            </div>
-            <div className="black-gradiant"></div>
-            <div className="NewComponentBlock">
-                {data.map((item, index) => (
-                    <NewsPageAnimes key={index} {...item} onClick={() => handleNewsPageAnimesClick(item.anime)}/>
-                ))}
-            </div>
-        </div>
-    );
+      <div
+        className="NewsTitleDesc"
+        style={{ backgroundImage: `url(${dataA.thumbnail})` }}
+      >
+        <h1>{dataA.title}</h1>
+        <p>
+          {dataA.description?.length > 500
+            ? dataA.description.substring(0, 500) + "..."
+            : dataA.description}
+        </p>
+      </div>
+      <div className="NewComponentBlock">
+        {data.map((item, index) => (
+          <NewsPageAnimes
+            key={index}
+            {...item}
+                onClick={() => handleNewsPageAnimesClick(item.anime)}
+                onMouseEnter={() => handleNewsPageAnimesClick(item.anime)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default News;
